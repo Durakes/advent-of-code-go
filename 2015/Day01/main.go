e89c29dee081c04e535efdf9db6892d181db4e68
Durakes <arnold.pajuelo@gmail.com>
2025-01-04 08:08:01 +0200

    Day 01 - 2015

--- 2015/Day01/main.go
+++ 2015/Day01/main.go
@@ -1,7 +1,47 @@
 package main
 
-import "fmt"
+import (
+	_ "embed"
+	"fmt"
+	"strings"
+)
+
+//go:embed input.txt
+var input string
 
 func main() {
-	fmt.Println("Hello, World!")
+	input = strings.TrimRight(input, "\n")
+	part1(input)
+	part2(input)
+
+}
+
+func part1(input string) {
+	result := 0
+	for _, letter := range input {
+		if letter == '(' {
+			result++
+		} else {
+			result--
+		}
+	}
+	fmt.Printf("Output 1: %d \n", result)
+}
+
+func part2(input string) {
+	result := 0
+	firstFloor := 0
+	for i, letter := range input {
+		if letter == '(' {
+			result++
+		} else {
+			result--
+		}
+
+		if result == -1 {
+			firstFloor = i + 1
+			break
+		}
+	}
+	fmt.Printf("Output 2: %d", firstFloor)
 }
